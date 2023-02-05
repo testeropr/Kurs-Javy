5b8ad85f77fedf8b9eb049d98df5841d6f9ab9d1
Tester Oprogramowania Daria <daria28.01@wp.pl>
2023-02-05 18:11:57 +0100

    instrukcja warunkowa

---
+++ src/InstrukcjaWarunkowaElseIf.java
@@ -0,0 +1,19 @@
+public class InstrukcjaWarunkowaElseIf {
+
+    public static void main(String[] args) {
+
+        int number = 12;
+
+        if (number == 0) {
+            System.out.println("Liczba rowna zero");
+        } else if (number > 0) {
+            System.out.println("Liczba dodatnia");
+        } else if (number > 10){
+            System.out.println("Numer większy od 10");
+        } else if (number < -5) {
+            System.out.println("Liczba mniejsza od -5");
+        } else {
+            System.out.println("Zaden warunek nie zostal spelniony");
+        }
+    }
+}
