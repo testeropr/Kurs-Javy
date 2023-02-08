0f3b6bf628a8209faaf9b34e076b445261feecde
Tester Oprogramowania Daria <daria28.01@wp.pl>
2023-02-08 18:40:50 +0100

    Pętla for

---
+++ src/ForLoop.java
@@ -0,0 +1,14 @@
+public class ForLoop {
+
+    public static void main(String[] args) {
+/*
+        for (int i = 0; i < 100; i = i + 2) {
+            System.out.println(i);
+        }*/
+        for (int i = 0; i < 100; i++) {
+            if (i % 5 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
