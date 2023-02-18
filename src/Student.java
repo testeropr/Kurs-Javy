27b19272bb14d4edb00dbb571e9eb00bad630e7e
Tester Oprogramowania Daria <daria28.01@wp.pl>
2023-02-18 17:28:34 +0100

    sample

---
+++ src/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+
+    public String imię;
+    public String nazwisko;
+    public String nick;
+    public String email;
+    public int numerIndeksu;
+
+    public void PrzedstawSię () {
+        System.out.println("Przedstaw się: " + imię + " " + nazwisko);
+    }
+    public void ZalogujSię () {
+        System.out.println("Zaloguj się za pomoca" + nick);
+    }
+    public void PodajNrIndeksu () {
+        System.out.println("Moj numer indeksu to: " + numerIndeksu);
+    }
+    public void PodajEmail () {
+        System.out.println("Moj email to: " + email);
+    }
+}
