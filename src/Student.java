50970ca28d5ad81476813a269f094b61964fed51
Tester Oprogramowania Daria <daria28.01@wp.pl>
2023-03-03 13:46:07 +0100

    sample git commit

--- src/Student.java
+++ src/Student.java
@@ -6,6 +6,10 @@ public class Student {
     public String email;
     public int numerIndeksu;
 
+    public static String nazwaUczelni = "AGH po zmianie";
+
+
+
     public void PrzedstawSię () {
         System.out.println("Przedstaw się: " + imię + " " + nazwisko);
     }
@@ -17,6 +21,15 @@ public class Student {
     }
     public void PodajEmail () {
         System.out.println("Moj email to: " + email);
+    }
+
+    public static void infoUczelnia(){
+        System.out.println("Moja uczelnia to "+ nazwaUczelni);
+        druga();
+
+        }
+        public static void druga(){
+            System.out.println("Jestem drugą metodą");
     }
 
 
