6e6752a8225ba83c1e5bc15622d5215198dd8fa6
Tester Oprogramowania Daria <daria28.01@wp.pl>
2023-03-26 19:14:58 +0200

    git commit

---
+++ src/Fibonacci.java
@@ -0,0 +1,25 @@
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        fibo(1);
+        fibo(2);
+        fibo(5);
+        fibo(6);
+    }
+
+    // n1 = 1, n2 = 1, n3=n1+n2
+
+    public static void fibo(int n){
+        long n1=1;
+        long n2=1;
+        for (int i =3; i<=n; i++){
+            long temp = n2+n1;
+            n1=n2;
+            n2 = temp;
+
+        }
+        System.out.println(n2);
+
+
+    }
+}
---
+++ src/Palindrom.java
@@ -0,0 +1,20 @@
+public class Palindrom {
+
+    // kajak
+    public static void main(String[] args) {
+        System.out.println(isPalindrom("kaja"));
+
+    }
+
+
+    public static boolean isPalindrom (String word) {
+        int n = word.length();
+        for (int i=0; i <(n/2); i++){
+            if (word.charAt(i) != word.charAt(n-i-1)) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
